cff3930dd260304296274a4d460dadc1e83e0b72
rapincx <rap.inc.ua@gmail.com>
2016-11-27 07:05:33 +0200

    Update tabs content and all other sections, and add functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,18 @@
  */
 $(document).ready(function(){
     $("section.menu h3").click(function(){
-        $("ul#left_menu").fadeToggle(1000);
+        $("ul#left_menu").slideToggle(500);
+    });
+    var panel = $(".auth-panel");
+    $('.tabs ul').on('click', 'li:not(.active)', function() {
+        $(this)
+            .addClass('active').siblings().removeClass('active')
+            .closest('body').find('div.main').removeClass('active').eq($(this).index()).addClass('active');
+    });
+    panel.on('click', '.reg', function() {
+        $(this).toggleClass("link").siblings().removeClass("link").closest("header").find(".auth-panel").toggleClass("active").closest("header").find(".registration").slideToggle();
+    });
+    panel.on('click', '.autht', function() {
+        $(this).toggleClass("link").siblings().removeClass("link").closest("header").find(".auth-panel").toggleClass("active").closest("header").find(".auth").slideToggle();
     });
 });
